d69e1468c1054a5fc2e2c44331d1e3c07c3c69b4
Charpell <charpellumeh@gmail.com>
2018-09-17 17:24:04 +0100

    Hashing Password with bycrypt

--- models/User.js
+++ models/User.js
@@ -26,4 +26,19 @@ const UserSchema = new Schema({
   }
 });
 
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
+});
+
 module.exports = mongoose.model("User", UserSchema);
