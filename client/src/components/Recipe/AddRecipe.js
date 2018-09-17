8e4fd4de0de4224b4a953de40950407a0d8c92c0
Charpell <charpellumeh@gmail.com>
2018-09-18 00:53:15 +0100

    Add Route Protection with WithAuth Component

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 import { Mutation } from "react-apollo";
 import { ADD_RECIPE, GET_ALL_RECIPES } from "../../queries";
 import Error from "../Error";
+import withAuth from "../withAuth";
 
 const initialState = {
   name: "",
@@ -135,4 +136,6 @@ class AddRecipe extends React.Component {
   }
 };
 
-export default withRouter(AddRecipe);
+export default withAuth(session => session && session.getCurrentUser)(
+  withRouter(AddRecipe)
+);
