e4411c0a3e963b10be6e1d75ebeb09a89ef3931d
Charpell <charpellumeh@gmail.com>
2018-09-17 18:10:19 +0100

    SignUp functunality completed

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,50 +1,102 @@
-import React, { Component } from 'react'
+import React from "react";
+import { withRouter } from "react-router-dom";
 
+import { Mutation } from "react-apollo";
+import Error from "../Error";
+import { SIGNUP_USER } from "../../queries";
 
-class Signup extends Component {
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  passwordConfirmation: ""
+};
+
+class Signup extends React.Component {
+  state = { ...initialState };
+
+  clearState = () => {
+    this.setState({ ...initialState });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event, signupUser) => {
+    event.preventDefault();
+    signupUser().then(async ({ data }) => {
+      console.log(data);
+    });
+  };
+
+  validateForm = () => {
+    const { username, email, password, passwordConfirmation } = this.state;
+    const isInvalid =
+      !username || !email || !password || password !== passwordConfirmation;
+    return isInvalid;
+  };
+
   render() {
-    return(
+    const { username, email, password, passwordConfirmation } = this.state;
+
+    return (
       <div className="App">
-        <h2 className="App">Signup</h2>    
-          <form
-            className="form"
-            
-          >
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email Address"
-              
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-             
-            />
-            <input
-              type="password"
-              name="passwordConfirmation"
-              placeholder="Confirm Password"
-              
-            />
-            <button
-              type="submit"
-              className="button-primary"
-            >
-              Submit
-            </button>
-          </form>
-        
+        <h2 className="App">Signup</h2>
+        <Mutation
+          mutation={SIGNUP_USER}
+          variables={{ username, email, password }}
+        >
+          {(signupUser, { data, loading, error }) => {
+            return (
+              <form
+                className="form"
+                onSubmit={event => this.handleSubmit(event, signupUser)}
+              >
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  name="passwordConfirmation"
+                  placeholder="Confirm Password"
+                  value={passwordConfirmation}
+                  onChange={this.handleChange}
+                />
+                <button
+                  type="submit"
+                  disabled={loading || this.validateForm()}
+                  className="button-primary"
+                >
+                  Submit
+                </button>
+                {error && <Error error={error} />}
+              </form>
+            );
+          }}
+        </Mutation>
       </div>
-    )
+    );
   }
 }
 
-export default Signup;
+export default withRouter(Signup);
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -10,3 +10,11 @@ export const GET_ALL_RECIPES = gql`
     }
   }
 `;
+
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`;
