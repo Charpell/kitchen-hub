037a45437f6bae24ec9a23a13ed8ba7aa7887569
Charpell <charpellumeh@gmail.com>
2018-09-17 17:32:03 +0100

    Configure react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import './index.css';
+
 import App from './components/App';
+import Signin from "./components/Auth/Signin";
+import Signup from "./components/Auth/Signup";
 
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
@@ -11,9 +20,23 @@ const client = new ApolloClient({
   uri: "http://localhost:4444/graphql"
 });
 
+
+const Root = ({ refetch, session }) => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" render={() => <Signin refetch={refetch} />} />
+      <Route path="/signup" render={() => <Signup refetch={refetch} />} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
+
+
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById("root")
 );
