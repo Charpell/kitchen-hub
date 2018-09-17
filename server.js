5b9a2f48486736390a7d131684b6c8c6acbbf3b4
Charpell <charpellumeh@gmail.com>
2018-09-17 15:34:25 +0100

    Configure Server and set up Database

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "variables.env" });
+
+// Connects to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
+
+
+  // Initializes application
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
