8473020517a91b778ff0fec69bc52dff64129909
Charpell <charpellumeh@gmail.com>
2018-09-18 09:57:37 +0100

    Display Recipe Image on Home Page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,15 +10,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Home</h1>
+        <h1 className="main-title">
+          Find Recipes You <strong>Love</strong>
+        </h1>
 
         <Query query={GET_ALL_RECIPES}>
           {({ data, loading, error }) => {
             if (loading) return <div>Loading</div>;
             if (error) return <div>Error</div>;
-
+              console.log(data)
             return (
-              <ul>
+              <ul className="cards">
                 {data.getAllRecipes.map(recipe => (
                   <RecipeItem key={recipe._id} {...recipe} />
                 ))}
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -7,6 +7,7 @@ export const GET_ALL_RECIPES = gql`
   query {
     getAllRecipes {
       _id
+      imageUrl
       name
       category
     }
