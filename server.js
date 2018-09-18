348f1b65a1bba97085473b8d8f4a2c7657be6390
Charpell <charpellumeh@gmail.com>
2018-09-18 08:58:52 +0100

    Preparation for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
@@ -57,7 +58,7 @@ app.use(async (req, res, next) => {
 
 
 // Create GraphiQL application
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+// app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 // Connect schemas with GraphQL
 app.use(
@@ -72,6 +73,16 @@ app.use(
     }
   }))
 );
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 const PORT = process.env.PORT || 4444;
 
